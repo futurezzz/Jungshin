ccaecb05e7752c60172893bb276e1d26b1b5e86c
futurezzz <79406446+futurezzz@users.noreply.github.com>
2021-07-17 23:39:31 +0900

    opening css, js update

--- js/opening.js
+++ js/opening.js
@@ -11,13 +11,16 @@ mainBtn.addEventListener('click',() => {
   if (clickCount === 1 ){
     mainText.style.color = 'transparent';
     setTimeout(() => {
-      boldText.style.color = '#345'
+      boldText.style.display = 'block';
+      boldText.classList.add('appear');
     },500);
   } else if (clickCount === 2 ){
     boldText.style.color = 'transparent';
     background.style.opacity = 0;
     setTimeout(() => {
-      logo.style.opacity = 1;
+      // logo.style.opacity = 1;
+      logo.style.display = 'block';
+      logo.classList.add('appear');
       mainBtn.textContent = '다음으로';
     },1000);
   } else {
